733918d00b0fb0a986c83f7d1bd0f5a5c808500b
Pakaz1 <61023046+Pakaz1@users.noreply.github.com>
2021-09-17 09:52:31 +0300

    Updated DbContext

--- RestSystemBackend/RestSystemBackend/DBContext/ApplicationDbContext.cs
+++ RestSystemBackend/RestSystemBackend/DBContext/ApplicationDbContext.cs
@@ -15,10 +15,14 @@ namespace RestSystemBackend.DBContext
 
         }
         public DbSet<User> Users { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Level> Levels { get; set; }
+        public DbSet<Achievement> Achievements { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<User>(entity => { entity.HasIndex(e => e.Email).IsUnique(); });
+            //builder.Entity<Game>().ToTable("Games");
         }
 
     }
